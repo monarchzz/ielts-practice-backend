a036602fe9c35c9e5fc57d482938484f0cfcba7b
TrungHieu <trunghieunguyendn@gmail.com>
2022-10-30 00:57:02 +0700

    fix

--- EFCore/Common/SeedHelper.cs
+++ EFCore/Common/SeedHelper.cs
@@ -4,18 +4,18 @@ namespace EFCore.Common;
 
 public class SeedHelper
 {
-    public static IEnumerable<TEntity> SeedData<TEntity>(string fileName)
-    {
-        var currentDirectory = Directory.GetCurrentDirectory();
-        var projectPath = currentDirectory[..currentDirectory.IndexOf("Api", StringComparison.Ordinal)];
-        var fullPath = Path.Combine(projectPath, "EFCore",
-            fileName.TrimStart('~').Replace('/', Path.DirectorySeparatorChar));
-        Console.WriteLine(fullPath);
-
-        using var reader = new StreamReader(fullPath);
-        var json = reader.ReadToEnd();
-        var result = JsonConvert.DeserializeObject<List<TEntity>>(json);
-
-        return result ?? new List<TEntity>();
-    }
+    // public static IEnumerable<TEntity> SeedData<TEntity>(string fileName)
+    // {
+    //     var currentDirectory = Directory.GetCurrentDirectory();
+    //     var projectPath = currentDirectory[..currentDirectory.IndexOf("Api", StringComparison.Ordinal)];
+    //     var fullPath = Path.Combine(projectPath, "EFCore",
+    //         fileName.TrimStart('~').Replace('/', Path.DirectorySeparatorChar));
+    //     Console.WriteLine(fullPath);
+    //
+    //     using var reader = new StreamReader(fullPath);
+    //     var json = reader.ReadToEnd();
+    //     var result = JsonConvert.DeserializeObject<List<TEntity>>(json);
+    //
+    //     return result ?? new List<TEntity>();
+    // }
 }
